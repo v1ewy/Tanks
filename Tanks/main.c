6dcd8bd050c5b40ba3483e37678bd85740b41f6d
Vedenov Ivan <fo4ikvanka@gmail.com>
2026-03-17 16:40:15 +0300

    Add proj files

---
+++ Tanks/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Tanks
+//
+//  Created by Иван Веденов on 17.03.2026.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
